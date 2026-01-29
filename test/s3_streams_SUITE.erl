f0b8f40faf281774c73a4ca7914a2ab97e7d396a
Lois Soto Lopez <lois@84codes.com>
2026-01-29 11:08:13 +0100

    Foramt and add stream clear in s3 after deletion

--- test/s3_streams_SUITE.erl
+++ test/s3_streams_SUITE.erl
@@ -23,28 +23,36 @@ init_per_suite(Config) ->
     rabbit_ct_helpers:log_environment(),
     Config1 = rabbit_ct_helpers:set_config(Config, [
         {rmq_nodename_suffix, ?MODULE}
-      ]),
+    ]),
     % Configure Osiris to use S3 components and set the S3 API backend
-    Config2 = rabbit_ct_helpers:merge_app_env(Config1,
-        [{osiris, [
-            {log_reader, rabbitmq_stream_s3_log_reader},
-            {log_manifest, rabbitmq_stream_s3_log_manifest}
-         ]},
-         {rabbitmq_stream_s3, [
-            {rabbitmq_stream_s3_api, rabbitmq_stream_s3_api_fs}
-         ]},
-         {rabbit, [
-            {max_message_size, 134217728}
-         ]}]),
-    Config3 = rabbit_ct_helpers:run_setup_steps(Config2,
-      rabbit_ct_broker_helpers:setup_steps() ++
-      rabbit_ct_client_helpers:setup_steps()),
+    Config2 = rabbit_ct_helpers:merge_app_env(
+        Config1,
+        [
+            {osiris, [
+                {log_reader, rabbitmq_stream_s3_log_reader},
+                {log_manifest, rabbitmq_stream_s3_log_manifest}
+            ]},
+            {rabbitmq_stream_s3, [
+                {rabbitmq_stream_s3_api, rabbitmq_stream_s3_api_fs}
+            ]},
+            {rabbit, [
+                {max_message_size, 134217728}
+            ]}
+        ]
+    ),
+    Config3 = rabbit_ct_helpers:run_setup_steps(
+        Config2,
+        rabbit_ct_broker_helpers:setup_steps() ++
+            rabbit_ct_client_helpers:setup_steps()
+    ),
     Config3.
 
 end_per_suite(Config) ->
-    rabbit_ct_helpers:run_teardown_steps(Config,
-      rabbit_ct_client_helpers:teardown_steps() ++
-      rabbit_ct_broker_helpers:teardown_steps()).
+    rabbit_ct_helpers:run_teardown_steps(
+        Config,
+        rabbit_ct_client_helpers:teardown_steps() ++
+            rabbit_ct_broker_helpers:teardown_steps()
+    ).
 
 init_per_testcase(Testcase, Config) ->
     rabbit_ct_helpers:testcase_started(Config, Testcase).
@@ -57,8 +65,8 @@ end_per_testcase(Testcase, Config) ->
 %% -------------------------------------------------------------------
 publish_consume(Config) ->
     % Generate payloads
-    Payload1K = <<0:(1024*8)>>,
-    Payload64M = <<1:(64*1024*1024*8)>>,
+    Payload1K = <<0:(1024 * 8)>>,
+    Payload64M = <<1:(64 * 1024 * 1024 * 8)>>,
 
     % Open channel
     Ch = rabbit_ct_client_helpers:open_channel(Config),
@@ -67,31 +75,66 @@ publish_consume(Config) ->
     QName = <<"stream_1">>,
 
     % Should not be data for the stream we haven't declared yet
-    ?assertMatch({error, not_found}, rabbit_ct_broker_helpers:rpc(Config, 0,
-                                            rabbitmq_stream_s3_api_fs,
-                                            get_stream_data, [QName])),
+    ?assertMatch(
+        {error, not_found},
+        rabbit_ct_broker_helpers:rpc(
+            Config,
+            0,
+            rabbitmq_stream_s3_api_fs,
+            get_stream_data,
+            [QName]
+        )
+    ),
 
     % Create a stream.
     ?assertEqual(ok, stream_declare(Ch, QName)),
-    ?assertEqual(ok, amqp_channel:call(Ch, #'basic.publish'{routing_key = QName},
-                                       #amqp_msg{payload = Payload1K})),
+    ?assertEqual(
+        ok,
+        amqp_channel:call(
+            Ch,
+            #'basic.publish'{routing_key = QName},
+            #amqp_msg{payload = Payload1K}
+        )
+    ),
 
     % Publishing a >= 64MB message triggers upload, since there is already a
     % chunk (the 1KB one) in the stream.
-    ?assertEqual(ok, amqp_channel:call(Ch, #'basic.publish'{routing_key = QName},
-                                       #amqp_msg{payload = Payload64M})),
-
+    ?assertEqual(
+        ok,
+        amqp_channel:call(
+            Ch,
+            #'basic.publish'{routing_key = QName},
+            #amqp_msg{payload = Payload64M}
+        )
+    ),
 
     % One fragment should exist
-    ?awaitMatch({ok,_Manifest,[_Fragment]},
-                rabbit_ct_broker_helpers:rpc(Config, 0,
-                                             rabbitmq_stream_s3_api_fs,
-                                             get_stream_data, [QName]),
-               500),
+    ?awaitMatch(
+        {ok, _Manifest, [_Fragment]},
+        rabbit_ct_broker_helpers:rpc(
+            Config,
+            0,
+            rabbitmq_stream_s3_api_fs,
+            get_stream_data,
+            [QName]
+        ),
+        500
+    ),
 
     % If we delete the stream, the data in the tiered storage should eventually
     % be cleared out.
     amqp_channel:call(Ch, #'queue.delete'{queue = QName}),
+
+    ?assertMatch(
+        {error, not_found},
+        rabbit_ct_broker_helpers:rpc(
+            Config,
+            0,
+            rabbitmq_stream_s3_api_fs,
+            get_stream_data,
+            [QName]
+        )
+    ),
 
     rabbit_ct_client_helpers:close_channel(Ch),
     ok.
@@ -102,11 +145,15 @@ publish_consume(Config) ->
 stream_declare(Ch, StreamName) ->
     Args = [{<<"x-queue-type">>, longstr, <<"stream">>}],
 
-    case amqp_channel:call(Ch,
-                           #'queue.declare'{
-                              queue = StreamName,
-                              durable = true,
-                              arguments = Args})
+    case
+        amqp_channel:call(
+            Ch,
+            #'queue.declare'{
+                queue = StreamName,
+                durable = true,
+                arguments = Args
+            }
+        )
     of
         #'queue.declare_ok'{} -> ok;
         Error -> {error, Error}
